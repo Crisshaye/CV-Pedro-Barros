c86b5a5c27ae85809b8dc534efe8da241967b6b8
Crisshaye <haye.cristobal@gmail.com>
2025-12-02 23:11:42 -0300

    fix final: añado Tailwind correctamente

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,29 @@
 module.exports = {
   siteMetadata: {
     title: `CV - Pedro Barros`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://pedrobarrosc.netlify.app`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: "images",
+        path: "./src/images/"
+      },
+      __key: "images"
     },
-    __key: "images"
-  }]
+    {
+      resolve: "gatsby-plugin-postcss",
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("autoprefixer")
+        ]
+      }
+    }
+  ]
 };
